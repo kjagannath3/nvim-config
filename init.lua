9608fc242ed46c6130f122aeeba9daebbd35c780
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-06-23 17:41:53 -0400

    second try at commit

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -19,5 +20,9 @@ vim.opt.rtp:prepend(lazypath)
 
 
 local opts = {}
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 require("options")
 require("lazy").setup("plugins")
+
