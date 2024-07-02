e717ecdd47e685c8075bf8e800ebce95e3353bae
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-07-01 23:17:06 -0400

    Primeagen settings

--- init.lua
+++ init.lua
@@ -24,5 +24,6 @@ local opts = {}
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 require("options")
+require("set")
 require("lazy").setup("plugins")
 
